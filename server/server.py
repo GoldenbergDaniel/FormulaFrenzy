068f5bd579a52433858dd808df22d183f8c6dd7f
John Lins <johnpaullins@gmail.com>
2021-02-26 18:00:18 -0800

    Link up

--- server/server.py
+++ server/server.py
@@ -1,1 +1,23 @@
-#
+from algorithum import *
+
+initInputArr()
+print("Random input list: ", inputsGlobal)
+
+
+
+
+functionGlobal = generateFunc(4)
+
+functionGlobal_display = ''.join(map(str, functionGlobal))
+""""x" +"""
+
+print("Function: ", functionGlobal_display)
+
+
+
+
+outputsGlobal = generateOutput(inputsGlobal, functionGlobal)
+print("Solved: ", outputsGlobal)
+
+
+print("With Parentheses: ", ''.join(map(str, withParentheses(functionGlobal))))
