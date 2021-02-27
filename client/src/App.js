a780d9234531d99052c8ca69399ee2a7db4103ae
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2021-02-27 11:27:51 -0500

    React WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,23 @@
+import React, { useState, useEffect} from "react"
+import io from "socket-io"
 import './App.css'
 
-function App() {
+var endPoint = "http://localhost:5000"
+var socket = io.connect(`${endPoint}`)
+
+var App = () => {
+  var [messages, setMessages] = useState([
+    "Hello and welcome!"
+  ])
+
+  var [messages, setMessages] = useState(["Hello and welcome!"])
+  var [message, setMessage] = useState("")
+
+  useEffect(() => {
+    getMessages()
+    [message.length]
+  })
+
   return (
     <div className="App">
       
