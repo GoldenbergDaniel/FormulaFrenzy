ffef0ae139057554f5808bc35ffa2e2638069b56
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2021-02-27 20:50:33 -0500

    SO MANY CHANGES

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect} from "react"
+import React, { useState} from "react"
 import io from "socket.io-client"
 import "./css/index.css"
 import "./css/App.css"
 
 import Navbar from "./components/Navbar"
+import MultiplayerSelect from "./components/MultiplayerSelect"
+import Singleplayer from "./components/Singleplayer"
+import Multiplayer from "./components/Multiplayer"
 
 var endPoint = "http://localhost:5000"
 var socket = io.connect(`${endPoint}`)
@@ -12,6 +15,9 @@ var App = () => {
   return (
     <div id="app">
       <Navbar/>
+      {/* <MultiplayerSelect/>a */}
+      {/* <Singleplayer/> */}
+      <Multiplayer/>
     </div>
   )
 }
--- client/src/components/Singleplayer.js
+++ client/src/components/Singleplayer.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect} from "react"
 import io from "socket.io-client"
 import "../css/singleplayer.css"
+import "../css/index.css"
 
 var Singleplayer = () => {  
   return (
     <div id="singleplayer">
-
+      <div id="table" className="white">
+        <div id="input1" className="left top">12</div>
+        <div id="input2" className="top">3</div>
+        <div id="input3" className="top">4</div>
+        <div id="input4" className="top">12</div>
+        <div id="output1" className="left">5</div>
+        <div id="output2">2</div>
+        <div id="output3">0</div>
+        <div id="output4">3</div>
+      </div>
+      <div id="input-box">
+        <input type="text" placeholder="Enter function" id="id-field" className="orange"></input>
+      </div>
     </div>
   )
 }
